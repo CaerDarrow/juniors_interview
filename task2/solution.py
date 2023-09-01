14596c936ff11e5b9df5ed13569dfd06c105b21f
pavelmaks <pavel651@yandex.ru>
2023-09-02 00:36:13 +0300

    add tests and fix prints

--- task2/solution.py
+++ task2/solution.py
@@ -19,7 +19,6 @@ async def get_count_of_animals(soup: BeautifulSoup) -> dict[str, int]:
     all_groups = mw_pages.find_all('div', class_='mw-category-group')
     for group in all_groups:
         main_char = group.find('h3').text
-        print(main_char)
         animals_links = group.find_all('a', href=re.compile(f'{main_char}*'))
         animals_counter[main_char] = len(animals_links)
     return animals_counter
@@ -55,10 +54,32 @@ async def main() -> None:
             if not next_page:
                 break
             url = bas_url + next_page
-        print(animals_counter.total())
         char_animal_list = sorted(animals_counter.items())
         write_data(char_animal_list)
 
 
+async def test_count_animals_on_page():
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+        animals_counter = Counter()
+        url = 'https://ru.wikipedia.org/wiki/Категория:Животные_по_алфавиту'
+        page = await get_page(session, url)
+        soup = BeautifulSoup(page, 'html.parser')
+        count_of_animals = await get_count_of_animals(soup)
+        animals_counter.update(count_of_animals)
+
+        assert animals_counter.total() == 200
+
+        bas_url = 'https://ru.wikipedia.org/'
+        next_page = await get_next_page(soup)
+        url = bas_url + next_page
+        page = await get_page(session, url)
+        soup = BeautifulSoup(page, 'html.parser')
+        count_of_animals = await get_count_of_animals(soup)
+        animals_counter.update(count_of_animals)
+
+        assert animals_counter.total() == 400
+
+
 if __name__ == '__main__':
+    asyncio.run(test_count_animals_on_page())
     asyncio.run(main())
