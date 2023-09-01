e37bdd4d35992c8cac64349bab0eb89c40ee1e5f
Valerij Ezhov <ezhov.fzzz@gmail.com>
2023-09-01 17:56:29 +0600

    тесты

--- tests/task2_test.py
+++ tests/task2_test.py
@@ -1,17 +1,42 @@
+import re
+from collections import defaultdict
+
 import aiohttp
 import pytest
 import aioresponses
-from task2.solution import fetch_page  # Импортируйте вашу функцию из вашего модуля
+from bs4 import BeautifulSoup
+
+from task2.solution import fetch_page
 
 
 @pytest.mark.asyncio
 async def test_fetch_page():
-    url = 'https://example.com'
+    url = 'https://ru.wikipedia.org/wiki/Категория:Животные_по_алфавиту'
 
+    # Имитируем страницу с одним животным на букву "А"
+    mock_page_content = '''
+    <div class="mw-category-group">
+        <ul>
+            <li><a>Антилопа</a></li>
+        </ul>
+    </div>
+    '''
+
+    # Имитация ответа сервера
     with aioresponses.aioresponses() as m:
-        m.get(url, payload={'data': 'some data'})
+        m.get(url, status=200, body=mock_page_content)
 
         async with aiohttp.ClientSession() as session:
-            response = await fetch_page(session, url)
+            response_text = await fetch_page(session, url)
+            soup = BeautifulSoup(response_text, 'html.parser')
 
-        assert response['data'] == 'some data'
+            animals_count = defaultdict(int)
+            for item in soup.select(".mw-category-group ul li a"):
+                animal_name = item.get_text().strip()
+                first_letter = animal_name[0].upper()
+                if not re.match("[А-ЯЁ]", first_letter):
+                    continue
+                animals_count[first_letter] += 1
+
+    # Проверка, что количество животных на букву "А" увеличилось на 1
+    assert animals_count["А"] == 1
