eb1edd5c7e4de2c65aa86241f9fc911ebc70fbc8
Valerij Ezhov <ezhov.fzzz@gmail.com>
2023-09-01 17:55:52 +0600

    решение первой задачи

--- task1/solution.py
+++ task1/solution.py
@@ -3,14 +3,14 @@ def strict(func):
         annotations = func.__annotations__
 
         arg_names = list(annotations.keys())
-
+        # для позиционных аргументов
         for i, arg in enumerate(args):
             arg_name = arg_names[i]
             expected_type = annotations[arg_name]
             if not isinstance(arg, expected_type):
                 raise TypeError(
                     f"Аргумент '{arg_name}' должен быть типа '{expected_type.__name__}', а получен '{type(arg).__name__}'")
-
+        # для именованных
         for arg_name, arg_value in kwargs.items():
             if arg_name in annotations:
                 expected_type = annotations[arg_name]
