f38cda39bdaa1b14d1824a2177a6184656b3a117
LightLM <97543843+LightLM@users.noreply.github.com>
2023-09-02 03:28:54 +0300

    Update 2 solution.py

--- task2/2 solution.py
+++ task2/2 solution.py
@@ -7,12 +7,6 @@ HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
 }
 
-
-def get_html(url):
-    r = requests.post(url, headers=HEADERS)
-    return r
-
-
 def parse_aph_links():
     result = []
     res = get_html(URL)
