9bac73c1df9db6c2fb4087a95eaada301f1e2a42
Kirill Kirsanov <montego@yandex.ru>
2023-09-04 23:26:38 +0300

    Code reformatted

--- task1/test_task1.py
+++ task1/test_task1.py
@@ -1,17 +1,21 @@
 import pytest
 from task1.solution import strict
 
-@strict
-def sum_two_int(a:int, b: int) -> int:
-    return a + b
 
 @strict
-def sum_two_float(a:float, b:float) -> float:
+def sum_two_int(a: int, b: int) -> int:
     return a + b
+
+
+@strict
+def sum_two_float(a: float, b: float) -> float:
+    return a + b
+
 
 @strict
 def sum_two_mix(a: int, b: float) -> float:
     return a + b
+
 
 @strict
 def bool_reverse(a: bool) -> bool:
@@ -34,6 +38,7 @@ def test_sum_two_mix():
     assert sum_two_mix(1, 2.5) == 3.5
     with pytest.raises(TypeError):
         sum_two_mix(1, 2)
+
 
 def test_bool_reverse():
     assert bool_reverse(True) == False
--- task3/solution.py
+++ task3/solution.py
@@ -1,12 +1,14 @@
 def appearance(intervals: dict[str, list[int]]) -> int:
-    lesson_beggining = intervals['lesson'][0]
-    lesson_end = intervals['lesson'][1]
+    lesson_beggining = intervals["lesson"][0]
+    lesson_end = intervals["lesson"][1]
     total_time = 0
-    tutor = normalize_intervals(intervals['tutor'])
-    pupil = normalize_intervals(intervals['pupil'])
+    tutor = normalize_intervals(intervals["tutor"])
+    pupil = normalize_intervals(intervals["pupil"])
     for i in range(0, len(tutor), 2):
         for j in range(0, len(pupil), 2):
-            delta = min(tutor[i+1], pupil[j+1], lesson_end) - max(tutor[i], pupil[j], lesson_beggining) 
+            delta = min(tutor[i + 1], pupil[j + 1], lesson_end) - max(
+                tutor[i], pupil[j], lesson_beggining
+            )
             total_time += delta if delta > 0 else 0
     return total_time
 
@@ -16,18 +18,25 @@ def normalize_intervals(intervals):
     for i in range(0, len(intervals), 2):
         applied = False
         for j in range(0, len(normalized_intervals), 2):
-            if min(normalized_intervals[j+1], intervals[i+1]) - max(normalized_intervals[j], intervals[i]) > 0:
+            if (
+                min(normalized_intervals[j + 1], intervals[i + 1])
+                - max(normalized_intervals[j], intervals[i])
+                > 0
+            ):
                 applied = True
-                if intervals[i] >= normalized_intervals[j] and intervals[i+1] >= normalized_intervals[j+1]:
-                    normalized_intervals.append(normalized_intervals[j+1])
-                    normalized_intervals.append(intervals[i+1])
-                elif intervals[i] <= normalized_intervals[j] and intervals[i+1] <= normalized_intervals[j+1]:
+                if (
+                    intervals[i] >= normalized_intervals[j]
+                    and intervals[i + 1] >= normalized_intervals[j + 1]
+                ):
+                    normalized_intervals.append(normalized_intervals[j + 1])
+                    normalized_intervals.append(intervals[i + 1])
+                elif (
+                    intervals[i] <= normalized_intervals[j]
+                    and intervals[i + 1] <= normalized_intervals[j + 1]
+                ):
                     normalized_intervals.append(intervals[i])
                     normalized_intervals.append(normalized_intervals[j])
         if not applied:
             normalized_intervals.append(intervals[i])
-            normalized_intervals.append(intervals[i+1])
+            normalized_intervals.append(intervals[i + 1])
     return normalized_intervals
-
-
-
