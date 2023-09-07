b91865130a40b2e5c57590c73cef107a93e71eac
radiantded <78826566+radiantded@users.noreply.github.com>
2023-09-07 03:51:00 -0600

    update solution

--- task1/solution.py
+++ task1/solution.py
@@ -3,7 +3,7 @@
 def strict(func):
     def inner(*args):
         z = list(zip(args, func.__annotations__.values()))
-        if not all(isinstance(x[0], x[1]) for x in z):
+        if not all(type(x[0]) == x[1] for x in z):
             raise TypeError(
                 f'Несовпадение типов данных: {z}'
             )
