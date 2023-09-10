843d1cb84bbf811de74312894c627fd82b3a72fe
DimasTius <100424548+DimasTius@users.noreply.github.com>
2023-09-10 15:15:33 +0300

    Create solution.py

---
+++ task1/solution.py
@@ -0,0 +1,8 @@
+def strict(func):
+    def wrapper(*args):# функция обертка
+        arg_annt = (v for v in func.__annotations__.values())# генератор состоящий из типов объявленных в прототипе декорируемой функции
+        if all(isinstance(obj, t) for obj, t in zip(args, arg_annt)):
+            return func(*args)
+        else:
+            raise TypeError
+    return wrapper
