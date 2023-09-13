ebbfc529d42dd19c2a6f9873e48296dd2c11b57a
DimasTius <100424548+DimasTius@users.noreply.github.com>
2023-09-13 22:24:05 +0300

    Update solution.py

--- task1/solution.py
+++ task1/solution.py
@@ -1,7 +1,7 @@
 def strict(func):
     def wrapper(*args):# функция обертка
         arg_annt = (v for v in func.__annotations__.values())# генератор состоящий из типов объявленных в прототипе декорируемой функции
-        if all(isinstance(obj, t) for obj, t in zip(args, arg_annt)):
+        if all(isinstance(bool, t) if type(obj) is bool else isinstance(obj, t) for obj, t in zip(args, arg_annt)):
             return func(*args)
         else:
             raise TypeError
