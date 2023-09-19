a94f325ec08957ce025877f88615ac913a5af49f
DimasTius <100424548+DimasTius@users.noreply.github.com>
2023-09-19 12:00:02 +0300

    add kwargs

--- task1/solution.py
+++ task1/solution.py
@@ -1,8 +1,13 @@
 def strict(func):
-    def wrapper(*args):# функция обертка
+    def wrapper(*args, **kwargs):# функция обертка
+        args = list(args) + list(kwargs.values())
         arg_annt = (v for v in func.__annotations__.values())# генератор состоящий из типов объявленных в прототипе декорируемой функции
         if all(isinstance(bool, t) if type(obj) is bool else isinstance(obj, t) for obj, t in zip(args, arg_annt)):
             return func(*args)
         else:
             raise TypeError
     return wrapper
+
+@strict
+def sum_two(a: int, b: int) -> int:
+    return a + b
